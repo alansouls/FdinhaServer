0cd58d2f6239f6b0ad47b41b9533806a2cc93bf5
alanAval <alan@avaltech.com.br>
2020-05-07 22:10:44 -0300

    InitialCommit

---
+++ src/FdinhaServer/FdinhaServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FdinhaServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
